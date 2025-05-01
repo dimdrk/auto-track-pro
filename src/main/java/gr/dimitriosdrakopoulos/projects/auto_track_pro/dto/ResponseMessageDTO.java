d76360e9e11f1f2a21dff325bbefe06cacecf4b6
DimDrk <dimdrk@gmail.com>
2025-05-01 20:07:56 +0300

    ResponseMessageDTO created

---
+++ src/main/java/gr/dimitriosdrakopoulos/projects/auto_track_pro/dto/ResponseMessageDTO.java
@@ -0,0 +1,16 @@
+package gr.dimitriosdrakopoulos.projects.auto_track_pro.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseMessageDTO {
+    private String code;
+    private String description;
+
+    public ResponseMessageDTO(String code) {
+        this.code = code;
+        this.description = "";
+    }
+}
