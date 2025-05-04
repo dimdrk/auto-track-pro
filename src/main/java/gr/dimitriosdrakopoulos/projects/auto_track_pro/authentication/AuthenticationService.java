de0eb8328f48caceb9559fc9fc1dfe7b6556b89e
DimDrk <dimdrk@gmail.com>
2025-05-04 18:07:56 +0300

    authenticationService created

---
+++ src/main/java/gr/dimitriosdrakopoulos/projects/auto_track_pro/authentication/AuthenticationService.java
@@ -0,0 +1,35 @@
+package gr.dimitriosdrakopoulos.projects.auto_track_pro.authentication;
+
+import gr.dimitriosdrakopoulos.projects.auto_track_pro.core.exceptions.AppObjectNotAuthorizedException;
+import gr.dimitriosdrakopoulos.projects.auto_track_pro.dto.AuthenticationRequestDTO;
+import gr.dimitriosdrakopoulos.projects.auto_track_pro.dto.AuthenticationResponseDTO;
+import gr.dimitriosdrakopoulos.projects.auto_track_pro.model.User;
+import gr.dimitriosdrakopoulos.projects.auto_track_pro.repository.UserRepository;
+import gr.dimitriosdrakopoulos.projects.auto_track_pro.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    public AuthenticationResponseDTO authenticate(AuthenticationRequestDTO dto)
+            throws AppObjectNotAuthorizedException {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword()));
+
+        User user = userRepository.findByUsername(authentication.getName())
+                .orElseThrow(() -> new AppObjectNotAuthorizedException("User", "User not authorized."));
+
+        String token = jwtService.generateToken(authentication.getName(), user.getRole().name());
+        return new AuthenticationResponseDTO(user.getFirstname(), user.getLastname(), token);
+    }
+}
