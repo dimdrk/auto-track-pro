187fcdee4407c58727e2e06c37bf0b49498ef90e
DimDrk <dimdrk@gmail.com>
2025-05-04 18:08:19 +0300

    customUserDetailService created

---
+++ src/main/java/gr/dimitriosdrakopoulos/projects/auto_track_pro/authentication/CustomUserDetailService.java
@@ -0,0 +1,22 @@
+package gr.dimitriosdrakopoulos.projects.auto_track_pro.authentication;
+
+import gr.dimitriosdrakopoulos.projects.auto_track_pro.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(()
+                -> new UsernameNotFoundException("User with username: " + username + " not found."));
+    }
+}
