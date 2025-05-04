6581306730fdc250842f48fe8a62d63b0dcbe293
DimDrk <dimdrk@gmail.com>
2025-05-04 18:09:17 +0300

    CustomAuthenticationEntryPoint created

---
+++ src/main/java/gr/dimitriosdrakopoulos/projects/auto_track_pro/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+package gr.dimitriosdrakopoulos.projects.auto_track_pro.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        // Set the response status 401 unauthorized
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        String json = "{\"code\":\"userNotAuthenticated\",\"description\": \"User must authenticate in order to access this endpoint.\"}";
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+    }
+}
