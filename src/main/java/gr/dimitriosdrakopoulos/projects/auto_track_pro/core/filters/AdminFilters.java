4ddef1e8f4dd62a11373a2c02e3eac8bae0a7062
DimDrk <dimdrk@gmail.com>
2025-05-04 17:58:49 +0300

    filters created

---
+++ src/main/java/gr/dimitriosdrakopoulos/projects/auto_track_pro/core/filters/AdminFilters.java
@@ -0,0 +1,31 @@
+package gr.dimitriosdrakopoulos.projects.auto_track_pro.core.filters;
+
+import lombok.*;
+import org.springframework.lang.Nullable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class AdminFilters extends GenericFilters {
+// TODO
+    @Nullable
+    private String uuid;
+
+    @Nullable
+    private String driverLicence;
+
+    @Nullable
+    private String identityNumber;
+
+    @Nullable
+    private String username;
+
+    @Nullable
+    private String userEmail;
+
+    @Nullable
+    private Boolean active;
+}
---
+++ src/main/java/gr/dimitriosdrakopoulos/projects/auto_track_pro/core/filters/DriverFilters.java
@@ -0,0 +1,28 @@
+package gr.dimitriosdrakopoulos.projects.auto_track_pro.core.filters;
+
+import lombok.*;
+import org.springframework.lang.Nullable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class DriverFilters extends GenericFilters {
+// TODO
+    @Nullable
+    private String uuid;
+
+    @Nullable
+    private String driverLicence;
+
+    @Nullable
+    private String username;
+
+    @Nullable
+    private String userEmail;
+
+    @Nullable
+    private Boolean active;
+}
